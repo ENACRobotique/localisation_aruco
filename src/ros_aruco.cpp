a499d2ecedde31521a345c7679c226dc4240660d
Florian Liehn <florian@liehn.com>
2017-07-15 20:33:54 +0200

    [aruco_cube] use cube size parameter in main

--- src/ros_aruco.cpp
+++ src/ros_aruco.cpp
@@ -97,8 +97,8 @@ int main(int argc,char **argv) {
 	ImageConverter ic  = ImageConverter(topic_pointeur);
     ros::Publisher pose_pub_markers = n.advertise<geometry_msgs::PoseStamped>("/aruco/markerarray", 1);
     cube_manager test_cube;
-    test_cube.push_back( aruco_cube(15) );
-    test_cube.push_back( aruco_cube(16) );
+    test_cube.push_back( aruco_cube(15,DEFAULT_CUBE_SIZE) );
+    test_cube.push_back( aruco_cube(16,DEFAULT_CUBE_SIZE) );
 
     //wait a image
     while (current_image.empty()) {
