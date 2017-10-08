84cbc264818325f52aa9302be71433ff762e5cdc
Florian Liehn <florian@liehn.com>
2017-10-08 11:12:09 +0200

    [Calibration] add executable and source

---
+++ src/calibration_aruco.cpp
@@ -0,0 +1,15 @@
+/*
+ * calibration_aruco.cpp
+ *
+ *  Created on: 8 oct. 2017
+ *      Author: liehnfl
+ */
+
+#include "aruco_cube.h"
+
+int main(int argc,char **argv) {
+
+	cout<<"all ok!"<<endl;
+
+
+}
