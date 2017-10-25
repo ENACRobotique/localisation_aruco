7e619c1d8719316bdadc3f20e15bd7a0247282db
EngelOfChipolata <engelofdeath@live.fr>
2017-10-25 22:48:18 +0200

    Adds Robots dedicated messages

---
+++ src/cube_pose_to_uart.cpp
@@ -0,0 +1,11 @@
+//
+// Created by guilhem on 25/10/17.
+//
+#include "ros/ros.h"
+
+#define NODE_NAME cube_pose_to_uart
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, NODE_NAME);
+}
+
