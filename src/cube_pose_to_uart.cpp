85649347ebc11975df2acd70be164a182d20f94e
EngelOfChipolata <engelofdeath@live.fr>
2017-11-13 22:23:30 +0100

    Don't use ArduPi anymore

--- src/cube_pose_to_uart.cpp
+++ src/cube_pose_to_uart.cpp
@@ -4,11 +4,17 @@
 #include <tf/transform_datatypes.h>
 #include "ros/ros.h"
 #include "Robots.h"
-#include "arduPi.h"
+#include <stdio.h>
+#include <unistd.h>			//Used for UART
+#include <fcntl.h>			//Used for UART
+#include <termios.h>		//Used for UART
+
 #define NODE_NAME "cube_pose_to_uart"
 #define SUB_ROBOTS_TOPIC "/robots"
 #define RADTOUINT16FACTOR 20860.756700940907
 #define MSG_SIZE 13
+
+int uart0_filestream = -1;
 
 typedef enum{
     MAIN_ALLY,
@@ -35,11 +41,68 @@ typedef union {
     char data[MSG_SIZE];
 } uRawMsg;
 
+void enable_uart(){
+
+    //-------------------------
+    //----- SETUP USART 0 -----
+    //-------------------------
+    //At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
+    uart0_filestream = -1;
+
+    //OPEN THE UART
+    //The flags (defined in fcntl.h):
+    //	Access modes (use 1 of these):
+    //		O_RDONLY - Open for reading only.
+    //		O_RDWR - Open for reading and writing.
+    //		O_WRONLY - Open for writing only.
+    //
+    //	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
+    //											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
+    //											immediately with a failure status if the output can't be written immediately.
+    //
+    //	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
+    uart0_filestream = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
+    if (uart0_filestream == -1)
+    {
+        //ERROR - CAN'T OPEN SERIAL PORT
+        printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
+    }
+
+    //CONFIGURE THE UART
+    //The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
+    //	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
+    //	CSIZE:- CS5, CS6, CS7, CS8
+    //	CLOCAL - Ignore modem status lines
+    //	CREAD - Enable receiver
+    //	IGNPAR = Ignore characters with parity errors
+    //	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
+    //	PARENB - Parity enable
+    //	PARODD - Odd parity (else even)
+    struct termios options;
+    tcgetattr(uart0_filestream, &options);
+    options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;		//<Set baud rate
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(uart0_filestream, TCIFLUSH);
+    tcsetattr(uart0_filestream, TCSANOW, &options);
+}
+
+void uart_write(char * data, int length){
+    if (uart0_filestream != -1)
+    {
+        ssize_t count = write(uart0_filestream, &data[0], static_cast<size_t>(length));		//Filestream, bytes to write, number of bytes to write
+        if (count < 0)
+        {
+            printf("UART TX error\n");
+        }
+    }
+}
+
 void newRobotPoseMessageCB(const cube_pos::Robots::ConstPtr& msg){
     for (auto robot : msg->robots) {
         uRawMsg rawMsg;
         sRobotMsg serialMsg{};
-        sRobotData pose{}, speed{};
         tf::Pose poseForQuaternionConversion;
         tf::poseMsgToTF(robot.pose.pose.pose, poseForQuaternionConversion);
         double yaw_angle = tf::getYaw(poseForQuaternionConversion.getRotation());
@@ -53,14 +116,14 @@ void newRobotPoseMessageCB(const cube_pos::Robots::ConstPtr& msg){
         serialMsg.robotSpeed.theta = static_cast<uint16_t>(robot.twist.twist.twist.angular.z);
 
         rawMsg.msg = serialMsg;
-        Serial.write(rawMsg.data, MSG_SIZE);
+        uart_write(rawMsg.data, MSG_SIZE);
 
     }
 }
 
 
 int main(int argc, char** argv) {
-    Serial.begin(9600);
+    enable_uart();
     ros::init(argc, argv, NODE_NAME);
     ros::NodeHandle n;
     ros::Subscriber s = n.subscribe<cube_pos::Robots>(SUB_ROBOTS_TOPIC, 1000, newRobotPoseMessageCB);
